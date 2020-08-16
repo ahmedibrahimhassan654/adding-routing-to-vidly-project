6b3f6ccfe6361b404f4b4a04007e451de1ba70b6
Mohamed Ibrahim <ahmedibrahimhassan654@gmail.com>
2020-08-16 03:51:37 +0200

    fix problem in nav bar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import { Route, Redirect, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 
-import Movies from './components/movies'
-import Customers from './components/customers.jsx'
+import Movies from "./components/movies";
+import Customers from "./components/customers.jsx";
 
-import Rentals from './components/rentals.jsx'
-import NotFound from './components/notFound.jsx'
-import NavBar from './components/navBar'
-import './App.css'
+import Rentals from "./components/rentals.jsx";
+import NotFound from "./components/notFound.jsx";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
-        <main className='container'>
+        <main className="container">
           <Switch>
-            <Route path='/customers' component={Customers}></Route>
-            <Route path='/movies' component={Movies}></Route>
-            <Route path='/rentals' component={Rentals}></Route>
-            <Route path='/not-found' component={NotFound}></Route>
-            <Redirect exact from='/' to='/movies' />
-            <Redirect to='/not-found' />
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="/not-found" />
           </Switch>
         </main>
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
