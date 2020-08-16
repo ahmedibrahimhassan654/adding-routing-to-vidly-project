2a0a5679451b075c024698dfeb847012286d7f50
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-08-16 02:43:36 +0200

    lec 16 adding Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
-import Movies from "./components/movies";
-import "./App.css";
+import React, { Component } from 'react'
+import { Route, Redirect,Switch } from 'react-router-dom'
+
+import Movies from './components/movies'
+import Customers from './components/customers.jsx'
+
+import Rentals from './components/rentals.jsx'
+import NotFound from './components/notFound.jsx'
+
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
+      <main className='container'>
+        <Switch>
+          <Route path='/customers' component={Customers}></Route>
+          <Route path='/movies' component={Movies}></Route>
+          <Route path='/rentals' component={Rentals}></Route>
+          <Route path='/not-found' component={NotFound}></Route>
+          <Redirect exact from='/' to='/movies' />
+          <Redirect to='/not-found'/>
+        </Switch>
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
